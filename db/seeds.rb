626e68b4a732810a680e6f3145568f6fea28fd0c
Charyl Idia <charyli@mailfence.com>
2024-10-08 16:02:19 +0100

    hooked up necklaces seed to index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'cleaning the database'
+Necklace.destroy_all
+User.destroy_all
+
+puts 'Creating Users...'
+user1 = User.create!(email: 'Dee123@zxc.com', password: 'dffhjg')
+user2 = User.create!(email: 'Lee456@zxc.com', password: 'dfhkghhjkj')
+user3 = User.create!(email: 'Jagoda789@zxc.com', password: 'rghfjgh')
+
+puts 'Finished!'
+
+puts 'Creating necklaces...'
+Necklace.create(name: 'Silver Cloud', price: 500.50, description: 'bridal', gemstone: 'ruby', material: 'pure silver', user: user1)
+
+
+Necklace.create(name: 'Goldeneye', price: 750.75, description: 'heavy', gemstone: 'saphire', material: 'Rose Gold', user: user1)
+
+
+Necklace.create(name: 'Diamond Geezer', price: 680.90, description: 'pure silver', gemstone: 'opal', material: 'silver', user: user2)
+
+
+puts "Done #{User.count} users and #{Necklace.count} necklaces created"
